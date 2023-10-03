d8b8ec3fb55d90bd62dffaad930b70197623bc01
mc4lucas <lucasross1234@gmail.com>
2023-10-02 21:29:32 -0400

    bug go away

--- lross53_LabExercise3/src/LA3Q1/DemoStackAndQueue_Lucas.java
+++ lross53_LabExercise3/src/LA3Q1/DemoStackAndQueue_Lucas.java
@@ -3,6 +3,16 @@ package LA3Q1;
 import java.util.Scanner;
 
 public class DemoStackAndQueue_Lucas {
+    //region Header & footer
+    public static String splitter = "======================================================="; //Storage var for the long splitter used in header and footer methods
+    public static void myHeader(int qtn){ //qtn is local
+        System.out.printf("%s\nLab Exercise 2-Q%d\nPrepared By: Lucas Ross\nStudent Number: 251276904\nGoal of this Exercise: To demonstrate the understanding of the Stack and Queue data\n%s\n",splitter,qtn,splitter); //Output
+    }
+
+    public static void myFooter(int qtn){ //qtn is local
+        System.out.printf("%s\nCompletion of Lab Exercise 3-Q%d is successful!\nSigning off - Lucas\n%s",splitter,qtn,splitter); //output
+    }
+    //endregion
     public static Scanner input = new Scanner(System.in); //Scanner to receive user input
 
     public static void stackDemo(LucasArray array){
@@ -48,7 +58,7 @@ public class DemoStackAndQueue_Lucas {
                    throw new Exception("");
                } //Exception Handling
             }catch(Exception e){
-                System.out.println(e + "Invalid Entry! Please choose one of these options below:");
+                System.out.println("Invalid Entry! Please choose one of these options below:");
                 input.nextLine();
             }
         }
@@ -97,13 +107,38 @@ public class DemoStackAndQueue_Lucas {
                     throw new Exception("");
                 } //Exception Handling
             }catch(Exception e){
-                System.out.println(e + "Invalid Entry! Please choose one of these options below:");
+                System.out.println("Invalid Entry! Please choose one of these options below:");
                 input.nextLine();
             }
         }
     }
 
     public static void main(String[] args){
-        stackDemo(new LucasArray());
+        myHeader(1); //Calling my header method
+
+        LucasArray x = new LucasArray(); //Creation of array
+
+        boolean exit = false; //Variable to end loop when user is done
+        while(!exit){
+            System.out.println("Main Demo-Menu:\n1) Stack\n2) Queue\n3) Exit");
+            try{ //Method used to validate entry
+                char entry = input.next().charAt(0);
+                if(entry == '1'){ //Push
+                   stackDemo(x);
+                } //For Push
+                else if (entry == '2') { //Pop
+                   queueDemo(x);
+                } //For Pop
+                else if (entry == '3') { //Exit
+                    exit = true; //end
+                } //For Exit
+                else{
+                    throw new Exception("");
+                } //Exception Handling
+            }catch(Exception e){
+                System.out.println("Invalid Entry! Please choose one of these options below:");
+                input.nextLine();
+            }
+        }
     }
 }
